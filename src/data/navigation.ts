2a7ad129d26d7526a3bb128f5d3493de7fdcab69
Alex Yoo <zerocustum89@gmail.com>
2026-02-21 20:45:07 -0800

    Update navigation.ts

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -1,10 +1,10 @@
 export const NAVIGATION_DATA = {
     navigationMenu: [
-        { href: "/alexanderyoo.github.io/home", label: "Home" },
-        { href: "/alexanderyoo.github.io/about", label: "About" },
-        { href: "/alexanderyoo.github.io/experience", label: "Experience" },
-        { href: "/alexanderyoo.github.io/projects", label: "Projects" },
-        { href: "/alexanderyoo.github.io/for-business", label: "For Business" },
-        { href: "/alexanderyoo.github.io/contact", label: "Contact" },
+        { href: "/home", label: "Home" },
+        { href: "/about", label: "About" },
+        { href: "/experience", label: "Experience" },
+        { href: "/projects", label: "Projects" },
+        { href: "/for-business", label: "For Business" },
+        { href: "/contact", label: "Contact" },
     ]
 };
