39753ef9cd014f579a1a3241ca9ad052f4c7dfce
Alex Yoo <zerocustum89@gmail.com>
2026-02-21 20:48:38 -0800

    Update navigation.ts

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -1,10 +1,10 @@
 export const NAVIGATION_DATA = {
     navigationMenu: [
-        { href: "/home", label: "Home" },
-        { href: "/about", label: "About" },
-        { href: "/experience", label: "Experience" },
-        { href: "/projects", label: "Projects" },
-        { href: "/for-business", label: "For Business" },
-        { href: "/contact", label: "Contact" },
+        { href: "#home", label: "Home" },
+        { href: "#about", label: "About" },
+        { href: "#experience", label: "Experience" },
+        { href: "#projects", label: "Projects" },
+        { href: "#for-business", label: "For Business" },
+        { href: "#contact", label: "Contact" },
     ]
 };
